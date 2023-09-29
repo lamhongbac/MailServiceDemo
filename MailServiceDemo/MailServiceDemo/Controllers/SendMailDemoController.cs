9717d4438228820c3103a9aa7266e76df5c60064
lam hong bac <lamhong.bac@gmail.com>
2023-09-29 15:14:33 +0700

    update code

---
+++ MailServiceDemo/MailServiceDemo/Controllers/SendMailDemoController.cs
@@ -0,0 +1,24 @@
+﻿using MailServiceDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace MailServiceDemo.Controllers
+{
+    public class SendMailDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SendMail()
+        {
+            SendMailModel model = new SendMailModel();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult SendMail(SendMailModel model)
+        {
+            return View();
+        }
+    }
+}
